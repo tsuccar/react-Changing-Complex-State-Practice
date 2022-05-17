57f13e7bb2c4d6f5181b7e5a529fdc94cdcfa167
tsuccar <tsuccar@users.noreply.github.com>
2022-05-16 20:06:33 -0400

    Completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,22 @@ function App() {
     email: ""
   });
 
+  function handleChange(event) {
+    // These two properties are deconstractued from the event.target
+    const { name, value } = event.target;
+    console.log(name);value={contact.fName} 
+    console.log(value);
+    setContact((preValue) => {
+      if (name === "fName") {
+        return { fName: value, lName: preValue.lName, email: preValue.email };
+      } else if (name === "lName") {
+        return { fName: preValue.fName, lName: value, email: preValue.email };
+      } else {
+        return { fName: preValue.fName, lName: preValue.lName, email: value };
+      }
+    });
+  }
+
   return (
     <div className="container">
       <h1>
@@ -14,9 +30,9 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input name="fName" value={contact.fName} onChange={handleChange} placeholder="First Name" />
+        <input name="lName" value={contact.lName} onChange={handleChange} placeholder="Last Name" />
+        <input name="email" value={contact.email} onChange={handleChange} placeholder="Email" />
         <button>Submit</button>
       </form>
     </div>
